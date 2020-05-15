35b51d5bc5ef8e89077194ad34d08a09837fbba0
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2020-05-15 18:04:46 +0100

    make rss-scraper use sqlite db

--- rss-scraper.py
+++ rss-scraper.py
@@ -2,13 +2,20 @@ import aiohttp, feedparser
 from asyncio import sleep,get_event_loop
 import async_timeout
 from datetime import datetime
+import aiosqlite
 
-##TODO: Transfer to either MongoDB or SQLite
-from json import load
-with open("sources.json") as file:
-    sources = load(file)
 firstRun = True
 feed = []
+
+class Source:
+    def __init__(self, data):
+        self.name = data[0]
+        self.short = data[1]
+        self.url = data[2]
+        self.icon = data[3]
+        self.descIgnore = data[4]
+    def __repr__(self):
+        return f"<Source \"{self.name}\">"
 
 class Article:
     def __init__(self, title, description, link, timestamp, sourceName, logo):
@@ -24,11 +31,17 @@ class Article:
         if type(other) is Article:
             return(self.link == other.link)
 
-async def fetchfeeds(loop, sources=None):
+async def get_sources():
+    async with aiosqlite.connect("db.sqlite3") as db:
+        async with db.execute("SELECT * FROM sources") as cursor:
+            return [Source(i) for i in await cursor.fetchall()]
+
+async def fetchfeeds(loop):
     results = []
+    sources = await get_sources()
     for i in sources:
         async with aiohttp.ClientSession(loop=loop) as session:
-            async with session.get(i["url"]) as response:
+            async with session.get(i.url) as response:
                 xml = await response.text()
                 rss = feedparser.parse(xml)
                 try:
@@ -38,19 +51,19 @@ async def fetchfeeds(loop, sources=None):
                 else:
                     result = Article(
                         selected.title, 
-                        selected.description if selected.get("description") and not i["descIgnore"] else "No description.", 
+                        selected.description if selected.get("description") and not i.descIgnore else "No description.", 
                         selected.link, 
                         selected.published_parsed,
-                        i["name"],
-                        i["icon"]
+                        i.name,
+                        i.icon
                     )
                     results.append(result)
     return results
 
 async def fetchall(loop):
-    global sources, firstRun, feed
+    global firstRun, feed
     while True:
-        results = await fetchfeeds(loop, sources)
+        results = await fetchfeeds(loop)
         for i in results:
             if i not in feed:
                 feed.append(i)
